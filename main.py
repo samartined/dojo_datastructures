0b19b89992ba9989e817157497c3a8ce4e44fb9d
Edgar <espinosamartinedgar@gmail.com>
2024-01-31 15:03:31 +0100

    Se modulariza la función para lector de csv

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-import csv
-
-
-def main():
-    with open("data.csv", "r") as archivo_csv_estudiantes:
-        lector_csv = csv.reader(archivo_csv_estudiantes)
-        encabezado_archivo = next(lector_csv)
-
-        dict_comprehension_estudiantes = [
-            {
-                encabezado_archivo[indice]: estudiante[indice]
-                for indice in range(len(encabezado_archivo))
-            }
-            for estudiante in lector_csv
-        ]
-    for estudiante in dict_comprehension_estudiantes:
-        print((estudiante))
-
+from lectorcsv import *
 
 if __name__ == "__main__":
-    main()
+    leer_csv()
